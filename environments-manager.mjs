2dbc1c23cf2c18c098bca891a67ff57c46139250
brn <56989493+brunosoouza@users.noreply.github.com>
2025-02-24 01:44:00 -0300

    ass

--- environments-manager.mjs
+++ environments-manager.mjs
@@ -3,10 +3,8 @@ import sodium from "libsodium-wrappers";
 
 const token = process.argv[2]; // O token deve ser o primeiro argumento
 const repo = process.argv[3]; // O repositório deve ser o segundo argumento
-const environments = process.argv[4]; // O JSON dos ambientes deve ser o terceiro 
+const environments = JSON.stringify(process.argv[4]); // O JSON dos ambientes deve ser o terceiro 
 const [owner, repository] = repo.split('/');
-
-console.log(Object.entries(environments))
 
 
 const octokit = new Octokit({ auth: token });
